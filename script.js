97e40f0482ff59cc1bbe35a0598fc0321908e8cd
mikicola <michela.colangelo@gmail.com>
2022-02-21 17:11:30 +0100

    output riuscito

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-
-let km = prompt('Km da percorrere?');
-let age = prompt('Quanti anni hai?');
-let outputEle = document.getElementById('my-output');
+let km = prompt("Km da percorrere?");
+let age = prompt("Quanti anni hai?");
 
 let pricePerKm = 0.21;
-let totalPrice = pricePerKm*km;
+let totalPrice = pricePerKm * km;
+
+if ((km) && (age)){
+  if(age < 18){
+    totalPrice = (totalPrice * 20) / 100;
+  }else if(age > 65){
+    totalPrice = (totalPrice * 40) / 100;
+  }
+
+
+    document.getElementById("my-output").innerHTML = "Il prezzo totale è " + totalPrice + "€.";
+}else{
+    document.getElementById("my-output").innerHTML = "Errore. Aggiorna la pagina e rispondi alle domande a schermo.";
+}
+
 
 // &&	and
 // ||	or
 // !	not
-
-if (age < 18){
-    totalPrice = totalPrice - 20%;
-}
-
-else if (age > 65){
-    totalPrice = totalPrice - 40%;
-}
-
-document.getElementById("output").innerHTML = "Il prezzo è di " + totalPrice;
-
-
-
-
-// let age1 = prompt('età 1:');
-// let age2 = prompt('età 2:');
-// let outputEle = document.getElementById('my-output');
-
-// if (age1 > age2) {
-//     outputEle.innerHTML = 'eta 1 maggiore';
-// }
-
-// else if (age1 < age2) {
-//     outputEle.innerHTML = 'eta 2 maggiore';
-// }
-
-// else {
-//     outputEle.innerHTML = 'eta uguale';
-// }
